ffd6c98e56c4ded680452279d4b455c8be4a464a
alzubeid <112045479+alzubeid@users.noreply.github.com>
2022-09-20 22:36:10 -0700

    Create ensg2hugo.py

---
+++ ensg2hugo.py
@@ -0,0 +1,30 @@
+%%file ensg2hugo.py
+import sys
+import fileinput
+import re
+import numpy as np 
+import pandas as pd
+
+#creating a dictionary
+Lookup_gene={}
+
+my_dict_file= sys.argv[1]
+Lookup_gene={}
+for each_line_of_text in fileinput.input("trgn_assignment3/Homo_sapiens.GRCh37.75.gtf.gz"):
+    gene=re.findall(r'^.*?\t.*?\t(gene)t', each_line_of_text, re.I)
+    gene_ID=re.findall(r'gene_id "(ENSG\d*?)"', each_line_of_text, re.I)
+    gene_name=r.findall(r'gene_name "(.*?)"', each_line_of_text, re.I)
+    homo_sapiens_split= re.split('\t', each_line_of_text)
+    if gene:
+        if gene_name:
+            Lookup_gene[gene_id[0]] = gene_name[0]
+ 
+
+    if sys.argv[1][:2]  == '-f':
+        columnnumber = sys.arg[1][2]
+        dataframe = sys.argv[2]
+        column = int(columnnumber) - 1
+    else: 
+        column = 1
+        data = sys.arv[1]
+        

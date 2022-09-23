6116fa570252e9d0364331e44ddf490380aaa1e5
alzubeid <112045479+alzubeid@users.noreply.github.com>
2022-09-23 14:25:10 -0700

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -9,7 +9,7 @@ x = np.random.normal(size=1000)
 plt.hist(x, density=True, bins=20)  # density=False would make counts
 plt.ylabel('Probability')
 plt.xlabel('Data');
-plt.title('Gaussian Distribution')
+plt.title('Gaussian Distribution in diabetics')
 
 # Save the histogram
 plt.savefig('hist.png')

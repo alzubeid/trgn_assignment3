ded2682e1e3f0b64f2b7839b2d168269ef6ab546
alzubeid <112045479+alzubeid@users.noreply.github.com>
2022-09-23 09:29:27 -0700

    Update ensg2hugo.py

--- ensg2hugo.py
+++ ensg2hugo.py
@@ -28,9 +28,26 @@ for each_line_of_text in fileinput.input("trgn_assignment3/Homo_sapiens.GRCh37.7
     else: 
         columnnumber = 0 #this is the first column (1-1=0)
         dataframe = sys.argv[1]
-  
-sample = open('final_expression_analysis', 'w')
-input_file_to_change=sys.argv[2]
-print('"","gene_id","gene_name","gene_type","logFC","AveExpr","t","P.Value","adj.P.Val"', file = sample)
-for each_line_of_text in fileinput.input(input_file_to_change): 
-    replacecolumn_array = re.replace(
+
+
+sample = open('dataframe', 'r')
+with open (dataframe, 'r') as file:
+    first_line = file.readline()
+    for line in file:
+        List = line.split(",")
+        match = re.match(r'^\w*")\.\w\",List[columnnumber]
+        if match:
+            gene_id = match.group(1)
+            if lookup_gene get.gene_id
+                List[columnnumber] = '"' + Lookup_gene[gene_id] + '"'
+            else:
+                List[columnnumber] = "not found"
+        else: 
+            print("not found")
+                         
+
+                         
+#input_file_to_change=sys.argv[2]
+#print('"","gene_id","gene_name","gene_type","logFC","AveExpr","t","P.Value","adj.P.Val"', file = sample)
+#for each_line_of_text in fileinput.input(input_file_to_change): 
+ #   splitcolumn_array = re.split(','each_line_of_text.replace

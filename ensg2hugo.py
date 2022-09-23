5f341e3a65163097f65e9274675f523821951f96
alzubeid <112045479+alzubeid@users.noreply.github.com>
2022-09-22 22:08:00 -0700

    Update ensg2hugo.py

--- ensg2hugo.py
+++ ensg2hugo.py
@@ -2,36 +2,35 @@
 import sys
 import fileinput
 import re
-import numpy as np 
-import pandas as pd
+
 
 #creating a dictionary
-Lookup_gene={}
 
 my_dict_file= sys.argv[1]
 Lookup_gene={}
 for each_line_of_text in fileinput.input("trgn_assignment3/Homo_sapiens.GRCh37.75.gtf"):
-    gene=re.findall(r'^.*?\t.*?\t(gene)t', each_line_of_text, re.I)
-    gene_ID=re.findall(r'gene_id "(ENSG\d*?)"', each_line_of_text, re.I)
-    gene_name=r.findall(r'gene_name "(.*?)"', each_line_of_text, re.I)
-    homo_sapiens_split= re.split('\t', each_line_of_text)
-    if gene:
+    gene_id = re.findall(r'gene_id "(ENSG\d*?)"', each_line_of_text, re.I)
+    #print(gene_id)
+    gene_name = re.findall(r'gene_name "(.*?)"', each_line_of_text, re.I)
+    #print(gene_name)
+    if gene_id:
         if gene_name:
             Lookup_gene[gene_id[0]] = gene_name[0]
- 
+            #print(gene_id[0] + " : " + gene_name[0])
+   
 
-    if sys.argv[1][:2]  == '-f':
-        columnnumber = sys.arg[1][2]
+#Column option setup:
+    if len(sys.argv) > 2:
+        if sys.argv[1][:2]  == '-f':
+        columnnumber = int(sys.argv[1][2]) - 1
         dataframe = sys.argv[2]
-        column = int(columnnumber) - 1
+       
     else: 
-        column = 1
-        data = sys.arv[1]
+        columnnumber = 0 #this is the first column (1-1=0)
+        dataframe = sys.argv[1]
   
-#Using Pandas
-df = pd.DataFrame(raw_data, r)
-df['gene_id'].replace(['gene_name'], inplace=True)
-df
-
-#Data_replaced = data.rename(columns = {'gene_id' : 'gene_name'}
- #                      print (Data_replaced)
+sample = open('final_expression_analysis', 'w')
+input_file_to_change=sys.argv[2]
+print('"","gene_id","gene_name","gene_type","logFC","AveExpr","t","P.Value","adj.P.Val"', file = sample)
+for each_line_of_text in fileinput.input(input_file_to_change): 
+    replacecolumn_array = re.replace(
